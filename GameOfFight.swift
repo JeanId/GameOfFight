1c1558d5a461f1bd68bb62935d719d93d429ca67
JeanId <jbarottin@gmail.com>
2023-03-08 14:43:29 +0100

    startGame()
    rrefonte class Game()

--- GameOfFight.swift
+++ GameOfFight.swift
@@ -15,9 +15,7 @@ enum CharacterType:String {
     case Warrior, Magus, Colossus, Dwarf
 }
 
-enum IdPlayer {
-    case Player_A, Player_B
-}
+
 
 class Character {
     var characterName:String
@@ -69,43 +67,83 @@ class HealerCharacter:Character {
     }
 }
 
+
 class Player {
-    
-    var player:IdPlayer
+    var playerLabel:String = ""
     var team:[Character]=[]
     var isAliveCharacter:Bool = true
+   
     
-    init(player:IdPlayer) {
-        self.player = player
-    }
+   
     
     func createCharacter(characterName: String, type: CharacterType ){
         team.append(Character(characterName: characterName, type: type))
     }
 }
 
-class Game {
-    var playerA = Player(player: .Player_A)
-    var playerB = Player(player: .Player_B)
-    
-    
-    func sayWelcome() {
-        print("Bonjour bienvenue sur GameOfFight 💥 ")
-        print("****** Première étape constitution des équipes ******")
+class PlayerA:Player {
+    private static var playerA:Player? = nil
+    public static var shared:Player {
+        if playerA == nil {
+            playerA = Player()
+            playerA?.playerLabel = "Joueur A"
+        }
+        return playerA!
+    }
+    private override init() {
+        
     }
     
-    func createTeam(player:IdPlayer) {
-        var nameCharacter = ""
-        let currentPlayer = Player(player: player)
-       
-        print("")
-        print("\(getPlayerLabel(_:player)) : contitution de son équipe de 3 personnages")
+}
+
+class PlayerB:Player {
+    private static var playerB:Player? = nil
+    public static var shared:Player {
+        if playerB == nil {
+            playerB = Player()
+            playerB?.playerLabel = "Joueur B"
+        }
+        return playerB!
+    }
+    private override init() {
         
+    }
+}
+
+
+class Game {
+    private let playerA = PlayerA.shared
+    private let playerB = PlayerB.shared
+    
+    func startGame() {
+        sayWelcome()
+        createTeam(player: playerA)
+        displayTeam(player: playerA)
+        print("")
+    }
+    
+    private func sayWelcome() {
+        print("")
+        print("******************************************************")
+        print("* 🛡️ ⚔️  Bonjour et bienvenue sur GameOfFight  ⚔️ 🛡️  💥 *")
+        print("******************************************************")
+        print("")
+        print("****** Première étape constitution des équipes *******")
+    }
+    
+    private func createTeam(player:Player) {
+        var nameCharacter = ""
+               
+        print("")
+        print("")
+        print("\(player.playerLabel) : constitution de son équipe de 3 personnages")
+
         for i in 1...3 {
             var isGoodInput = true
             nameCharacter = inputNameCharacter(index: i)
             
             repeat {
+                print("")
                 print("""
                 Choisissez le type du personnage numéro \(i) en tapant le chiffre correspondant :
                 1. ⚔️  Warrior     L'attaquant classique (points de vie et arme équilibrés)
@@ -116,7 +154,7 @@ class Game {
                 
                 if let type = inputCharacterType() {
                     print("Type personnage numéro \(i) : \(type.rawValue)")
-                    currentPlayer.createCharacter(characterName: nameCharacter, type: type)
+                    player.createCharacter(characterName: nameCharacter, type: type)
                     isGoodInput = true
                 } else {
                     isGoodInput = false
@@ -127,26 +165,25 @@ class Game {
         
     }
     
-    func displayTeam(player:IdPlayer) {
-        let currentPlayer = Player(player: player)
-        
-        for character in currentPlayer.team {
-            print("Personnage : \(character.characterName) Type : \(character.type.rawValue) Points de vie : \(character.lifeValue) Force arme : \(character.weaponValue)")
+    private func displayTeam(player:Player) {
+        print("")
+        print("")
+        print("\(player.playerLabel) : récapitulatif de l'équipe constituée")
+        print("")
+     
+
+        for character in player.team {
+            print("""
+            Personnage    : \(character.characterName)
+            Type          : \(character.type.rawValue)
+            Points de vie : \(character.lifeValue)
+            Force arme    : \(character.weaponValue)
+            """)
+            print("")
         }
         
     }
                   
-    private func getPlayerLabel(_ player:IdPlayer) -> String {
-        var playerLabel = ""
-                
-        switch player {
-            case .Player_A:
-                playerLabel = "Joueur A"
-            case .Player_B:
-                playerLabel = "Joueur B"
-        }
-        return playerLabel
-    }
                   
     private func inputNameCharacter(index i:Int) -> String {
         var valid = true
@@ -199,7 +236,4 @@ class Game {
 
 
 var game = Game()
-game.sayWelcome()
-game.createTeam(player: .Player_A)
-game.displayTeam(player: .Player_A)
-//game.createTeam(player: .Player_B)
+game.startGame()
